e7adc8fd4723d6f7a457b95076b7c9ddae8c0e00
M F K <mfkfawas@gmail.com>
2021-10-08 15:04:17 +0530

    Using passport.js for level 6 authentication, to add cookies and sessions.

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const ejs = require('ejs')
 const mongoose = require('mongoose')
 //const encrypt = require('mongoose-encryption')
 //const md5 = require('md5')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
+//const bcrypt = require('bcrypt')
+//const saltRounds = 10
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const session = require('express-session')
+const connectEnsureLogin = require('connect-ensure-login');// authorization
 
 const app = express()
 app.use(express.static('public'))
@@ -18,6 +22,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 })
+userSchema.plugin(passportLocalMongoose)
 //Encryption keys in .env
 
 //it is important to add thid plugin above part1 bcoz in part1 we are paSSing userSchema to User model.
@@ -28,38 +33,94 @@ const userSchema = new mongoose.Schema({
 //part1
 const User = mongoose.model('user', userSchema)
 
+app.use(session({
+    secret: process.env.EXPRESS_SESSION_SECRETCODE,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 }
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+
+  passport.use(User.createStrategy());
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
 app.get('/', (req, res)=>{
     res.render('home')
 })
+
+app.get('/logout', (req, res)=>{
+    req.logout()
+    res.redirect('/')
+})
+
+// app.route('/secrets')
+// .get((req, res)=>{
+//     if(req.isAuthenticated()){
+//         res.render('secrets')
+//     } else{
+//         console.log("Not authenticated")
+//         res.redirect('/login')
+//     }
+// })
+
+//This codes are not from Angela Yu.Two days gone on above secrets route.
+//This code i from https://heynode.com/tutorial/authenticate-users-node-expressjs-and-passportjs/
+app.get('/secrets', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+    res.render('secrets')
+  });
+
 
 app.route('/login')
 .get((req, res)=>{
     res.render('login')
 })
 .post((req, res)=>{
-    
-    username = req.body.username
-    password = req.body.password
 
-    User.findOne({email: username}, (err, result)=>{
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
 
-        if(!err){
-            // Load hash from your password DB.
-            if(result){
-                bcrypt.compare(password, result.password, function(err, hashResult) {
-                    //this fn automatically converts password to its corresponding hash.
-                    // result == true
-                    if(hashResult){
-                        res.render('secrets')
-                    } else{
-                        res.send("Invalid Password")
-                    }
-                }) 
-            }  else{
-                res.send("User does not exist.")
-            } 
-        } else{
+
+//     // User.findOne({email: username}, (err, result)=>{
+
+//     //     if(!err){
+//     //         // Load hash from your password DB.
+//     //         if(result){
+//     //             bcrypt.compare(password, result.password, function(err, hashResult) {
+//     //                 //this fn automatically converts password to its corresponding hash.
+//     //                 // result == true
+//     //                 if(hashResult){
+//     //                     res.render('secrets')
+//     //                 } else{
+//     //                     res.send("Invalid Password")
+//     //                 }
+//     //             }) 
+//     //         }  else{
+//     //             res.send("User does not exist.")
+//     //         } 
+//     //     } else{
+//     //         console.log(err)
+//     //     }
+//     // })
+
+//     //aUTHENTICATION using passport.js
+    req.login(user, (err)=>{
+        if(err){
             console.log(err)
+        } else{
+                passport.authenticate('local')(req, res, ()=>{
+                res.redirect('/secrets')
+                })
         }
     })
 })
@@ -70,19 +131,29 @@ app.route('/register')
 })
 .post((req, res)=>{
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const registrer = new User({
-            email: req.body.username,
-            password: hash
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    //     // Store hash in your password DB.
+    //     const registrer = new User({
+    //         email: req.body.username,
+    //         password: hash
             
-        })
-        registrer.save()
-        res.redirect('/login')
-    });
+    //     })
+    //     registrer.save()
+    //     res.redirect('/login')
+    // });
 
-
-})
+    //Passport.js authentication
+    User.register({username: req.body.username}, req.body.password, (err, user)=>{
+        if(err){
+            console.log(err)
+            res.redirect('/register')
+        } else{
+            passport.authenticate('local')(req, res, ()=>{
+                res.redirect('/secrets')
+            })
+        }
+    })
+})                           
 
 
 
